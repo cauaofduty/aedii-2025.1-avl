ac333f3babab979bfc05af0e1ee1eab4ac16a30a
caua de SOUZA <cauasantos064@gmail.com>
2025-07-02 02:22:22 -0300

    rotações finalizadas (sem testar)

--- avl.c
+++ avl.c
@@ -19,70 +19,170 @@ node* createNode(int value){
     return newNode;
 }
 
-//rever
+
 node* rotateRight(node* root){
 
     node *a = root, *b = a->left, *c = b->right;
     
     b->right = a;
-    b->cb = 0;
-
 
     a->left = c;
     a->cb = 0;
 
     return b;
 }
-//rever
+
 node* rotateLeft(node* root){
     
     node *a = root, *b = a->right, *c = b->left;
 
     b->left = a;
-    b->cb = 0;
 
     a->right = c;
+
+    //o pivô tem seu cb atualizado
     a->cb = 0;
 
+    //a intenção é atualizar esse cb fora dessa função para não alterar cbs indevidamente numa rotação dupla
     return b;
     
 }
-//aqui
-node *rotateDoubleLeft(node *root){
 
+node *rotateRightLeft(node *root){
 
+    node *a = root, *b = a->right, *c = b->left; 
+
+    a->right = rotateRight(b);
+
+    //como na proxima rotação a nova raiz é c, que atualmente é filho de a:
+    c = rotateLeft(a);
+
+    //analiza cb antigo de c (que é a nova raiz), antes de alterar
+    switch (c->cb){
+        case 1:
+            a->cb = -1;
+            b->cb = 0;
+            break;
+
+        case 0:
+            b->cb = 0;
+            a->cb = 0;
+            break;
+
+        case -1:
+            b->cb = 1;
+            a->cb = 0;
+            break;
+        
+        default: //para fins de debug
+            printf("Erro de balanceamento\n");
+            break;
+    }
+    //corrige cb da nova raiz
+    c->cb = 0;
+    return c;
 }
 //aqui
 node *rotateLeftRight(node *root){
 
+    node *a = root, *b = a->left, *c = b->right;
 
+    a->left = rotateLeft(b);
 
+    //como na proxima rotação a nova raiz é c, que atualmente é filho de a:
+    c = rotateRight(a);
+
+    //analiza cb "antigo" de c
+    switch (c->cb){
+        case 1:
+            a->cb = 0;
+            b->cb = -1;
+            break;
+
+        case 0:
+            b->cb = 0;
+            a->cb = 0;
+            break;
+
+        case -1:
+            b->cb = 0;
+            a->cb = 1;
+            break;
+        
+        default: //para fins de debug
+            printf("Erro de balanceamento\n");
+            break;
+    }
+    //corrige cb da nova raiz
+    c->cb = 0;
+    return c;
 }
 
 
-node* balance(node* root){ //no com cb > = 2 // -2
-
-    //rotação à esquerda (desequilibrio a direita)
-    if(root->cb > 1 && root->right->cb > 0) root = rotateLeft(root);
-
-    else if(root->cb > 1 && root->right->cb < 0)
-  
-    return root;
-
-}
-
-node* insert(node *root, int value){
-    if(!root) return createNode(value); 
+//como orientado em aula, jeito mais simples para indicar desbalanceamento é utilizar ponteiro para flag que indica crescimento de lado da arvore
+node* insert(node *root, int value, int* raised){
+    if(!root){
+        *raised = 1;
+        return createNode(value); 
+    } 
     
     //direciona descida
-    if(value >= root->value) root->right = insert(root->right, value);
-     
-    else root->left = insert(root->left, value);
+    if(value > root->value) {
+        root->right = insert(root->right, value, raised);
+        
+        //verifica atualização de cb
+        if(raised){
+            switch (root->cb){
 
-    //atualiza cb e checa balanceamento
-    cbUpdate(root);
-    
-    return balance(root);
+            case -1:
+                root->cb = 0;
+                raised = 0;
+                break;
+
+            case 0:
+                root->cb = 1;
+                raised = 1;
+                break;
+
+            case 1:
+                raised = 0;
+                if()
+
+            default://via das duvidas
+                printf("erro em algum lugar no balanceamento\n");
+                break;
+            }
+        }
+    //caso inserção esquerda
+    } else {
+        root->left = insert(root->left, value, raised);
+
+        if(raised){
+            switch (root->cb) {
+            case -1: //para de propagar a verificação
+                raised = 0;
+                return rotate(root);
+
+            case 0:
+                root->cb = -1;
+                raised = 1;
+                break;
+
+            case 1:
+                raised = 0;
+                root->cb = 0;
+                break;
+
+            default://via das duvidas
+                printf("erro em algum lugar no balanceamento\n");
+                break;
+            }
+
+
+        }
+
+    }
+
 
 }
 
